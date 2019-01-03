68ed960fb9589c4fb6dd4ee4f8fdc327b59ab849
Mandy <mandy.lan@gmail.com>
2019-01-04 00:53:52 +1300

    any excercises

--- any_pattern_test.rb
+++ any_pattern_test.rb
@@ -23,38 +23,35 @@ class AnyPatternTest < Minitest::Test
   end
 
   def test_has_at_least_one_alice
-    skip
     names = ["Bill", "Bob", "Burton", "Alice", "Brandon"]
-    has_alice = false
-    # Your code goes here
+    has_alice = names.any? { |name| name == 'Alice' }
     assert has_alice
   end
 
   def test_no_alices
-    skip
     names = ["Chuck", "Charlene", "Cory", "Chris", "Carl"]
-    # Your code goes here
+    has_alice = names.any? { |name| name == 'Alice' }
     refute has_alice
   end
 
   def test_has_a_multi_word_phrase
-    skip
     phrases = ["Sure!", "OK.", "I have no idea.", "Really?Whatever."]
-    # Your code goes here
+    has_multi_word_phrase = false
+    phrases. each do |phrase|
+      has_multi_word_phrase = true if phrase.split(' ').length > 0
+    end
     assert has_multi_word_phrase
   end
 
   def test_no_monkeys
-    skip
     animals = ["elephant", "hippo", "jaguar", "python"]
-    # Your code goes here
+    has_monkeys = animals.any? {|animal| animals == 'monkey'}
     refute has_monkeys
   end
 
   def test_no_multiples_of_five
-    skip
     numbers = [3, 1, 3, 2, 4, 9, 8]
-    # Your code goes here
+    multiples_of_5 = numbers.any? {|number| number % 5 == 0 }
     refute multiples_of_5
   end
 
